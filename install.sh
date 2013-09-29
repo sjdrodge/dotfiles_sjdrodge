abffb929f3ed70d11ee3236075528a044ab0bf88
Stephen Drodge <stephen.drodge@gmail.com>
2013-09-28 20:42:53 -0400

    New installation procedure. No longer wipes out unmatched content in the top-level of directories. Should be extended to actually recursively apply this rule...

--- install.sh
+++ install.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
+#TODO: relative symlinks
 #TODO: create a symlink/cp flag (both options would be nice)
 #TODO: Fix cp symlink/directory bugs
+#TODO: Actually recurse on dirs.
 
-#TODO: find the dir correctly
-dir=~/src/dotfiles_tehminkeh
+pushd `dirname $0` > /dev/null
+sourcedir=`pwd -P`
+popd > /dev/null
+
 bkup_dir=~/.dotfiles_bkup
-excludes="install.sh"
+files="Xresources gitconfig inputrc tmux.conf vimrc zlogin zshrc.local"
+dirs="vim"
 
-#TODO: test if $bkup_dir exists and print what we're doing if it doesn't
+#TODO: test if $BKUP_DIR exists and print what we're doing if it doesn't
 mkdir -p $bkup_dir
 
-cd $dir
-for file in * ;do
-  isexcluded=0
-  for exclude in $excludes ;do
-    if [ "$file" == "$exclude" ] ;then
-      isexcluded=1
-    fi
-  done
-  if [ $isexcluded == 1 ] ;then
-    echo Ignoring "$file"
-  else
-    if [ -e ~/.$file ] ;then
-      echo Backing up "~/.$file" as "$bkup_dir/$file"
-      cp -r ~/.$file $bkup_dir/$file
-      rm -r ~/.$file
-    fi
-    echo Installing "$file" as "~/.$file"
-    ln -T -s $dir/$file ~/.$file
+for file in $files ;do
+  if [ -e ~/.$file ] ;then
+    echo Backing up "~/.$file" to "$bkup_dir/$file"
+    cp -r ~/.$file $bkup_dir/$file
+    rm -r ~/.$file $bkup_dir/$file
   fi
+  echo Installing "~/.$file"
+  ln -T -s $sourcedir/$file ~/.$file
+done
+
+for dir in $dirs ;do
+  if [ -e ~/.$dir ] ;then
+    echo Backing up contents of "~/.$dir" to "$bkup_dir/$dir"
+    cp -r ~/.$dir $bkup_dir/$dir
+  fi
+  mkdir -p $dir
+  for file in $sourcedir/$dir/* ;do
+    if [ -e ~/.$dir/$file ] ;then
+      rm -r ~/.$dir/$file
+    fi
+    echo Installing "~/.$dir/$file"
+    ln -T -s $sourcedir/$dir/$file ~/.$dir/$file
+  done
 done
